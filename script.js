7765188fec1f00ff48eb5a4dbdfff17f5df7d615
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2025-02-27 11:13:08 +0000

    add functionality to select number of rounds

--- script.js
+++ script.js
@@ -1,117 +1,138 @@
-let playerScore = 0;
-let computerScore = 0;
+const gameLaunchModal = document.querySelector("#game-launch-container");
+const roundSelectBtns = document.querySelectorAll(".round-select-btn");
+const playGameBtn = document.querySelector("#play-game-btn");
 
-function getComputerChoice() {
-  let randomNum = Math.floor(Math.random() * 3);
+roundSelectBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    roundSelectBtns.forEach((btn) => btn.classList.remove("select"));
+    e.target.classList.add("select");
+    playGameBtn.classList.remove("inactive");
+  });
+});
 
-  if (randomNum == 0) {
-    return "rock";
-  } else if (randomNum == 1) {
-    return "paper";
-  } else {
-    return "scissors";
+playGameBtn.addEventListener("click", (e) => {
+  if (e.target.classList.contains("inactive")) return;
+
+  if (gameLaunchModal.classList.contains("reveal")) {
+    gameLaunchModal.classList.remove("reveal");
+    gameLaunchModal.classList.add("hidden");
   }
-}
+});
 
-function getPlayerChoice() {
-  while (true) {
-    let input = prompt("Please enter Rock, Paper, or Scissors").toLowerCase();
+// let playerScore = 0;
+// let computerScore = 0;
 
-    if (input != "rock" && input != "paper" && input != "scissors") {
-      alert("Invalid choice! Try again.");
-      continue;
-    } else {
-      return input;
-    }
-  }
-}
+// function getComputerChoice() {
+//   let randomNum = Math.floor(Math.random() * 3);
 
-function playRound(playerChoice, computerChoice) {
-  let playerWinMessage = "Congratulations, Player! You win!";
-  let computerWinMessage = "Bad luck, Player! You lose";
+//   if (randomNum == 0) {
+//     return "rock";
+//   } else if (randomNum == 1) {
+//     return "paper";
+//   } else {
+//     return "scissors";
+//   }
+// }
 
-  if (playerChoice == computerChoice) {
-    console.log(`You chose ${playerChoice}!`);
-    console.log(`The computer chose ${computerChoice}!`);
-    console.log("It's a draw! Play again");
-  } else if (playerChoice == "rock" && computerChoice == "paper") {
-    computerScore++;
-    console.log(`You chose ${playerChoice}!`);
-    console.log(`The computer chose ${computerChoice}!`);
-    console.log(computerWinMessage);
-    console.log(
-      `The scores are: Player ${playerScore} - ${computerScore} Computer`
-    );
-  } else if (playerChoice == "rock" && computerChoice == "scissors") {
-    playerScore++;
-    console.log(`You chose ${playerChoice}!`);
-    console.log(`The computer chose ${computerChoice}!`);
-    console.log(playerWinMessage);
-    console.log(
-      `The scores are: Player ${playerScore} - ${computerScore} Computer`
-    );
-  } else if (playerChoice == "paper" && computerChoice == "rock") {
-    playerScore++;
-    console.log(`You chose ${playerChoice}!`);
-    console.log(`The computer chose ${computerChoice}!`);
-    console.log(playerWinMessage);
-    console.log(
-      `The scores are: Player ${playerScore} - ${computerScore} Computer`
-    );
-  } else if (playerChoice == "paper" && computerChoice == "scissors") {
-    computerScore++;
-    console.log(`You chose ${playerChoice}!`);
-    console.log(`The computer chose ${computerChoice}!`);
-    console.log(computerWinMessage);
-    console.log(
-      `The scores are: Player ${playerScore} - ${computerScore} Computer`
-    );
-  } else if (playerChoice == "scissors" && computerChoice == "rock") {
-    computerScore++;
-    console.log(`You chose ${playerChoice}!`);
-    console.log(`The computer chose ${computerChoice}!`);
-    console.log(computerWinMessage);
-    console.log(
-      `The scores are: Player ${playerScore} - ${computerScore} Computer`
-    );
-  } else if (playerChoice == "scissors" && computerChoice == "paper") {
-    playerScoreScore++;
-    console.log(`You chose ${playerChoice}!`);
-    console.log(`The computer chose ${computerChoice}!`);
-    console.log(playerWinMessageWinMessage);
-    console.log(
-      `The scores are: Player ${playerScore} - ${computerScore} Computer`
-    );
-  } else {
-    console.log("Hmm, something's wrong...");
-  }
-}
+// function getPlayerChoice() {
+//   while (true) {
+//     let input = prompt("Please enter Rock, Paper, or Scissors").toLowerCase();
 
-function playGame() {
-  let roundCount = 1;
+//     if (input != "rock" && input != "paper" && input != "scissors") {
+//       alert("Invalid choice! Try again.");
+//       continue;
+//     } else {
+//       return input;
+//     }
+//   }
+// }
 
-  while (roundCount <= 5) {
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
+// function playRound(playerChoice, computerChoice) {
+//   let playerWinMessage = "Congratulations, Player! You win!";
+//   let computerWinMessage = "Bad luck, Player! You lose";
 
-    playRound(playerChoice, computerChoice);
+//   if (playerChoice == computerChoice) {
+//     console.log(`You chose ${playerChoice}!`);
+//     console.log(`The computer chose ${computerChoice}!`);
+//     console.log("It's a draw! Play again");
+//   } else if (playerChoice == "rock" && computerChoice == "paper") {
+//     computerScore++;
+//     console.log(`You chose ${playerChoice}!`);
+//     console.log(`The computer chose ${computerChoice}!`);
+//     console.log(computerWinMessage);
+//     console.log(
+//       `The scores are: Player ${playerScore} - ${computerScore} Computer`
+//     );
+//   } else if (playerChoice == "rock" && computerChoice == "scissors") {
+//     playerScore++;
+//     console.log(`You chose ${playerChoice}!`);
+//     console.log(`The computer chose ${computerChoice}!`);
+//     console.log(playerWinMessage);
+//     console.log(
+//       `The scores are: Player ${playerScore} - ${computerScore} Computer`
+//     );
+//   } else if (playerChoice == "paper" && computerChoice == "rock") {
+//     playerScore++;
+//     console.log(`You chose ${playerChoice}!`);
+//     console.log(`The computer chose ${computerChoice}!`);
+//     console.log(playerWinMessage);
+//     console.log(
+//       `The scores are: Player ${playerScore} - ${computerScore} Computer`
+//     );
+//   } else if (playerChoice == "paper" && computerChoice == "scissors") {
+//     computerScore++;
+//     console.log(`You chose ${playerChoice}!`);
+//     console.log(`The computer chose ${computerChoice}!`);
+//     console.log(computerWinMessage);
+//     console.log(
+//       `The scores are: Player ${playerScore} - ${computerScore} Computer`
+//     );
+//   } else if (playerChoice == "scissors" && computerChoice == "rock") {
+//     computerScore++;
+//     console.log(`You chose ${playerChoice}!`);
+//     console.log(`The computer chose ${computerChoice}!`);
+//     console.log(computerWinMessage);
+//     console.log(
+//       `The scores are: Player ${playerScore} - ${computerScore} Computer`
+//     );
+//   } else if (playerChoice == "scissors" && computerChoice == "paper") {
+//     playerScoreScore++;
+//     console.log(`You chose ${playerChoice}!`);
+//     console.log(`The computer chose ${computerChoice}!`);
+//     console.log(playerWinMessageWinMessage);
+//     console.log(
+//       `The scores are: Player ${playerScore} - ${computerScore} Computer`
+//     );
+//   } else {
+//     console.log("Hmm, something's wrong...");
+//   }
+// }
 
-    roundCount++;
-  }
+// function playGame() {
+//   let roundCount = 1;
 
-  console.log("That's the end of the game!");
-  console.log(
-    `The scores are: Player ${playerScore} - ${computerScore} Computer`
-  );
+//   while (roundCount <= 5) {
+//     let playerChoice = getPlayerChoice();
+//     let computerChoice = getComputerChoice();
 
-  if (playerScore > computerScore) {
-    console.log("Congratulations, Player! You beat the machine");
-  } else if (computerScore > playerScore) {
-    console.log("Bad luck, Player! Another win for the machines");
-  } else {
-    console.log("A draw! Rematch pending...");
-  }
-}
+//     playRound(playerChoice, computerChoice);
+
+//     roundCount++;
+//   }
+
+//   console.log("That's the end of the game!");
+//   console.log(
+//     `The scores are: Player ${playerScore} - ${computerScore} Computer`
+//   );
+
+//   if (playerScore > computerScore) {
+//     console.log("Congratulations, Player! You beat the machine");
+//   } else if (computerScore > playerScore) {
+//     console.log("Bad luck, Player! Another win for the machines");
+//   } else {
+//     console.log("A draw! Rematch pending...");
+//   }
+// }
 // let i = 0
 
 // while (i <= 5) {
